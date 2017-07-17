0e8459e3ae6ba76a07885c1f07466e588d992b63
Ruffeng <ruben.suet@gmail.com>
2017-07-17 20:21:13 +0200

    Added the chicken

--- myOwnGame/js/main.js
+++ myOwnGame/js/main.js
@@ -5,12 +5,16 @@ var GameState = {
   preload: function(){
     // params ( key, path)
     this.load.image('background', 'assets/images/background.png');
+    this.load.image('chicken', 'assets/images/chicken.png');
   },
   create: function (){
     // Creating instance var called background
     // we refer to the game variable and add a sprite
     // coords are(0 = X top, 0 = Y Left, the key of the image that we called background)
     this.background = this.game.add.sprite(0,0, 'background');
+    // to center it we can use the world helper that it returns the world
+    // of the game
+    this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY,'chicken');
   },
   update: function() {
 
