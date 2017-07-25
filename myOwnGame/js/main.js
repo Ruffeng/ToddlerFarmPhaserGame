687a69489b607f017697a7d3ae6cc15b39678af3
Ruffeng <ruben.suet@gmail.com>
2017-07-25 15:08:45 +0200

    Added some basics

--- myOwnGame/js/main.js
+++ myOwnGame/js/main.js
@@ -8,9 +8,17 @@ var GameState = {
     this.load.image('chicken', 'assets/images/chicken.png');
     this.load.image('horse', 'assets/images/horse.png');
     this.load.image('pig', 'assets/images/pig.png');
+    this.load.image('sheep', 'assets/images/sheep3.png');
+    this.load.image('arrow', 'assets/images/arrow.png');
 
   },
   create: function (){
+    // Making the world responsible to the screen
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    // centerint the world in the center as X as Y
+    this.scale.pageAlignHoritzontally = true ;
+    this.scale.pageAlignVertically = true ;
+
     // Creating instance var called background
     // we refer to the game variable and add a sprite
     // coords are(0 = X top, 0 = Y Left, the key of the image that we called background)
@@ -35,10 +43,46 @@ var GameState = {
 
     this.pig = this.game.add.sprite(500,300, 'pig');
     this.pig.anchor.setTo(0.5);
+    // If You put a negative number, you are fliping the image
     this.pig.scale.setTo(-1,1);
+
+
+    // Rotation
+    this.sheep = this.game.add.sprite(100,250,'sheep');
+    this.sheep.scale.setTo(0.5);
+    this.sheep.anchor.setTo(0.5);
+    // 0 by default. Positive number is clockwise. negative is counter clockwise
+    this.sheep.angle = -45;
+
+
+    // Basic input user
+    this.rightArrow = this.game.add.sprite(500,this.game.world.centerY, 'arrow');
+    this.rightArrow.anchor.setTo(0.5);
+    // Adding custom params for my program
+    this.rightArrow.customParams = { direction : 1 };
+    this.rightArrow.inputEnabled = true;
+    this.rightArrow.input.pixelPerfectClick = true;
+    this.rightArrow.events.onInputDown.add(this.switchAnimal, this);
+
+
+
+
+    this.leftArrow = this.game.add.sprite(60,this.game.world.centerY, 'arrow');
+    this.leftArrow.anchor.setTo(0.5);
+    this.leftArrow.scale.setTo(-1, 1);
+    // Adding custom params for my program
+    this.leftArrow.customParams = { direction : -1 };
+
+    // Adding a custom listener
+    this.leftArrow.inputEnabled = true;
+    this.leftArrow.input.pixelPerfectClick = true;
+    this.leftArrow.events.onInputDown.add(this.switchAnimal, this);
   },
   update: function() {
-
+    this.sheep.angle += 0.5;
+  },
+  switchAnimal: function(sprite, event){
+    console.log('pressed!');
   },
 };
 
